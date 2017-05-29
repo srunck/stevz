89c16914168cf18736ef0f64644bdbffb5c7022f
Steve R <srunck@q.com>
2017-05-29 00:16:05 -0500

    Added js folder and changed includes for nav and footer to javascript format.

---
+++ js/footer.js
@@ -0,0 +1,7 @@
+document.write('\
+\
+<div class="footer">\
+  <br><br><br>FOOTER<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>\
+</div>\
+\
+');
